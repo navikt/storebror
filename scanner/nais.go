7f8414d7f4b4625474e1dcc7e0157434e6c68a90
Kim Tore Jensen <kimtjen@gmail.com>
2018-06-14 11:41:55 +0200

    Patch nais app-config.yml with default Prometheus configuration.

--- scanner/nais.go
+++ scanner/nais.go
@@ -1,17 +1,69 @@
 package scanner
 
-import yaml "gopkg.in/yaml.v2"
+import (
+	"fmt"
+	"io/ioutil"
+	"strings"
+
+	yaml "gopkg.in/yaml.v2"
+)
 
 type Result struct {
 	err error
 }
 
+type Transformer func(Yaml) Yaml
+
 type Yaml map[interface{}]interface{}
 
+// merge adds src's Yaml structure over to dst.
+func merge(dst, src Yaml) {
+	for k, v := range src {
+		dst[k] = v
+	}
+}
+
+// shortImageName returns a best guess at the application's name.
+func shortImageName(longname string) string {
+	tokens := strings.Split(longname, "/")
+	return tokens[len(tokens)-1]
+}
+
+// ParseYamlFile parses a file's contents and returns a Yaml structure.
+func ParseYamlFile(filename string) (Yaml, error) {
+	data, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return nil, err
+	}
+	return ParseYaml(data)
+}
+
+// ParseYaml parses a byte stream and returns a Yaml structure.
 func ParseYaml(data []byte) (Yaml, error) {
 	m := make(Yaml)
 	err := yaml.Unmarshal(data, &m)
 	return m, err
+}
+
+// DefaultPrometheusConfiguration returns a sane, default Prometheus
+// configuration section that can be inserted into a Yaml structure.
+func DefaultPrometheusConfiguration(app string) (Yaml, error) {
+	template := `
+prometheus:
+  enabled: true
+  path: /%s/internal/metrics
+`
+	data := fmt.Sprintf(template, app)
+	return ParseYaml([]byte(data))
+}
+
+func InsertPrometheus(src Yaml) Yaml {
+	name := shortImageName(src["image"].(string))
+	result := make(Yaml)
+	merge(result, src)
+	defaultConfig, _ := DefaultPrometheusConfiguration(name)
+	merge(result, defaultConfig)
+	return result
 }
 
 func AppConfig(yml string) Result {
--- scanner/nais_test.go
+++ scanner/nais_test.go
@@ -8,14 +8,30 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestFoo(t *testing.T) {
+func TestParseYaml(t *testing.T) {
 	t.Run("Parse YAML file correctly", func(t *testing.T) {
 		data, err := ioutil.ReadFile("fixtures/app-config/base.yml")
-		if err != nil {
-			t.Fatalf("fixture is missing")
-		}
+		assert.Nil(t, err)
 		yml, err := scanner.ParseYaml(data)
 		assert.Equal(t, "foo", yml["image"])
 		assert.Equal(t, true, yml["prometheus"].(scanner.Yaml)["enabled"])
+	})
+}
+
+func TestPrometheus(t *testing.T) {
+	t.Run("Default Prometheus configuration is sane", func(t *testing.T) {
+		yml, err := scanner.DefaultPrometheusConfiguration("foo-app")
+		assert.Nil(t, err)
+		assert.Equal(t, true, yml["prometheus"].(scanner.Yaml)["enabled"])
+		assert.Equal(t, "/foo-app/internal/metrics", yml["prometheus"].(scanner.Yaml)["path"])
+	})
+
+	t.Run("Insert Prometheus configuration if absent", func(t *testing.T) {
+		src, err := scanner.ParseYamlFile("fixtures/app-config/prometheus.input.yml")
+		assert.Nil(t, err)
+		expected, err := scanner.ParseYamlFile("fixtures/app-config/prometheus.output.yml")
+		assert.Nil(t, err)
+		dst := scanner.InsertPrometheus(src)
+		assert.Equal(t, expected, dst)
 	})
 }
